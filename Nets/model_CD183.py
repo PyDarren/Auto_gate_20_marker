97bff543c86150dbbd54ca14ff61804de2ccd01a
GIT <chenda4701@163.com>
2020-01-09 14:25:42 +0800

    CD183模型

--- Nets/model_CD183.py
+++ Nets/model_CD183.py
@@ -67,8 +67,8 @@ if __name__ == "__main__":
     ######################################
     #### Data import
     data_path = 'E:/cd/0_Auto_Gate_20_marker/model_data/'
-    file_0 = 'CXCR5+'
-    file_1 = 'CXCR5-'
+    file_0 = 'CD183+'
+    file_1 = 'CD183-'
     markers = ('CD57', 'CD3', 'CD56', 'gdTCR', 'CCR6', 'CD14 ',
                'IgD', 'CD123(IL-3R)', 'CD85J', 'CD19', 'CD25', 'CD274(PD-L1)',
                'CD278(ICOS)', 'CD39', 'CD27', 'CD24', 'CD45RA', 'CD86', 'CD28',
@@ -119,7 +119,7 @@ if __name__ == "__main__":
 
     model.fit(train_X,
               train_labels,
-              epochs=10,
+              epochs=100,
               # batch_size=16384,
               # validation_data=(test_X, test_labels),
               # verbose=2
@@ -131,4 +131,7 @@ if __name__ == "__main__":
     print('\nTest less accuracy:', test_less_acc)
 
     ## save model
-    model.save('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CXCR5_classfy.h5')
+    model.save('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD183_classfy.h5')
+
+
+
