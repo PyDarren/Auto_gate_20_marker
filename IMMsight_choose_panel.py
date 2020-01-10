dfd525df4410675f2ebd08848061df67323ada47
GIT <chenda4701@163.com>
2020-01-10 09:20:53 +0800

    测试新加入部分marker

--- IMMsight_choose_panel.py
+++ IMMsight_choose_panel.py
@@ -88,8 +88,6 @@ if __name__ == '__main__':
                'CD183(CXCR3)', 'CD94', 'CD127(IL-7Ra)', 'CD279(PD-1)', 'CD38',
                'CD20', 'CD16', 'HLA-DR', 'CD4', 'CD8a', 'CD11b')
 
-
-
     new_samples_path = Fpath + 'WriteFcs/'
 
     CD3_Pos_list = list()
@@ -98,6 +96,31 @@ if __name__ == '__main__':
     CD56_Pos_list = list()
     gdTCR_Pos_list = list()
     CD8_Pos_list = list()
+    CD14_Pos_list = list()
+    Igd_Pos_list = list()
+    CD123_Pos_list = list()
+    CD85j_Pos_list = list()
+    CD19_Pos_list = list()
+    CD25_Pos_list = list()
+    CD39_Pos_list = list()
+    CD27_Pos_list = list()
+    CD24_Pos_list = list()
+    CD45RA_Pos_list = list()
+    CD86_Pos_list = list()
+    CD28_Pos_list = list()
+    CD197_Pos_list = list()
+    CD11c_Pos_list = list()
+    CD33_Pos_list = list()
+    CD152_Pos_list = list()
+    CD161_Pos_list = list()
+    CXCR5_Pos_list = list()
+    CD183_Pos_list = list()
+    CD94_Pos_list = list()
+    CD127_Pos_list = list()
+    PD1_Pos_list = list()
+    CD20_Pos_list = list()
+    CD16_Pos_list = list()
+    
 
     for info in os.listdir(new_samples_path):
         input_shape = (None, 36)
@@ -119,6 +142,80 @@ if __name__ == '__main__':
 
         model_CD8 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD8_classfy.h5')
         model_CD8.build(input_shape)
+        
+        model_CD14 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD14_classfy.h5')
+        model_CD14.build(input_shape)
+
+        model_Igd = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/Igd_classfy.h5')
+        model_Igd.build(input_shape)
+        
+        model_CD123 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD123_classfy.h5')
+        model_CD123.build(input_shape)
+        
+        model_CD85j = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD85j_classfy.h5')
+        model_CD85j.build(input_shape)
+
+        model_CD19 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD19_classfy.h5')
+        model_CD19.build(input_shape)
+
+        model_CD25 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD25_classfy.h5')
+        model_CD25.build(input_shape)
+
+        model_CD39 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD39_classfy.h5')
+        model_CD39.build(input_shape)
+
+        model_CD27 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD27_classfy.h5')
+        model_CD27.build(input_shape)
+
+        model_CD24 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD24_classfy.h5')
+        model_CD24.build(input_shape)
+
+        model_CD45RA = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD45RA_classfy.h5')
+        model_CD45RA.build(input_shape)
+
+        model_CD86 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD86_classfy.h5')
+        model_CD86.build(input_shape)
+
+        model_CD28 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD28_classfy.h5')
+        model_CD28.build(input_shape)
+
+        model_CD197 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD197_classfy.h5')
+        model_CD197.build(input_shape)
+
+        model_CD11c = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD11c_classfy.h5')
+        model_CD11c.build(input_shape)
+
+        model_CD33 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD33_classfy.h5')
+        model_CD33.build(input_shape)
+
+        model_CD152 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD152_classfy.h5')
+        model_CD152.build(input_shape)
+
+        model_CD161 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD161_classfy.h5')
+        model_CD161.build(input_shape)
+
+        model_CXCR5 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CXCR5_classfy.h5')
+        model_CXCR5.build(input_shape)
+
+        model_CD183 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD183_classfy.h5')
+        model_CD183.build(input_shape)
+
+        model_CD94 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD94_classfy.h5')
+        model_CD94.build(input_shape)
+
+        model_CD127 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD127_classfy.h5')
+        model_CD127.build(input_shape)
+
+        model_PD1 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/PD1_classfy.h5')
+        model_PD1.build(input_shape)
+
+        model_CD20 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD20_classfy.h5')
+        model_CD20.build(input_shape)
+
+        model_CD16 = tf.keras.models.load_model('C:/Users/pc/OneDrive/git_repo/Auto_gate_20_marker/Models/CD16_classfy.h5')
+        model_CD16.build(input_shape)
+
+
 
         sample_df = pd.read_csv(new_samples_path + info)
         sample_df = sample_df.loc[:, markers]
@@ -146,8 +243,102 @@ if __name__ == '__main__':
         new_df = copy.deepcopy(sample_df)
         ratio_CD8_all, CD8_df, CD8_labels = ratioCalculation2(new_df, model_CD8)
         CD8_Pos_list.append(ratio_CD8_all[0])
-
-
+        # CD14
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD14_all, CD14_df, CD14_labels = ratioCalculation2(new_df, model_CD14)
+        CD14_Pos_list.append(ratio_CD14_all[0])
+        # Igd
+        new_df = copy.deepcopy(sample_df)
+        ratio_Igd_all, Igd_df, Igd_labels = ratioCalculation2(new_df, model_Igd)
+        Igd_Pos_list.append(ratio_Igd_all[0])
+        # CD123
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD123_all, CD123_df, CD123_labels = ratioCalculation2(new_df, model_CD123)
+        CD123_Pos_list.append(ratio_CD123_all[0])
+        # CD85j
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD85j_all, CD85j_df, CD85j_labels = ratioCalculation2(new_df, model_CD85j)
+        CD85j_Pos_list.append(ratio_CD85j_all[0])
+        # CD19
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD19_all, CD19_df, CD19_labels = ratioCalculation2(new_df, model_CD19)
+        CD19_Pos_list.append(ratio_CD19_all[0])
+        # CD25
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD25_all, CD25_df, CD25_labels = ratioCalculation2(new_df, model_CD25)
+        CD25_Pos_list.append(ratio_CD25_all[0])
+        # CD39
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD39_all, CD39_df, CD39_labels = ratioCalculation2(new_df, model_CD39)
+        CD39_Pos_list.append(ratio_CD39_all[0])
+        # CD27
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD27_all, CD27_df, CD27_labels = ratioCalculation2(new_df, model_CD27)
+        CD27_Pos_list.append(ratio_CD27_all[0])
+        # CD24
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD24_all, CD24_df, CD24_labels = ratioCalculation2(new_df, model_CD24)
+        CD24_Pos_list.append(ratio_CD24_all[0])
+        # CD45RA
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD45RA_all, CD45RA_df, CD45RA_labels = ratioCalculation2(new_df, model_CD45RA)
+        CD45RA_Pos_list.append(ratio_CD45RA_all[0])
+        # CD86
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD86_all, CD86_df, CD86_labels = ratioCalculation2(new_df, model_CD86)
+        CD86_Pos_list.append(ratio_CD86_all[0])
+        # CD28
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD28_all, CD28_df, CD28_labels = ratioCalculation2(new_df, model_CD28)
+        CD28_Pos_list.append(ratio_CD28_all[0])
+        # CD197
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD197_all, CD197_df, CD197_labels = ratioCalculation2(new_df, model_CD197)
+        CD197_Pos_list.append(ratio_CD197_all[0])
+        # CD11c
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD11c_all, CD11c_df, CD11c_labels = ratioCalculation2(new_df, model_CD11c)
+        CD11c_Pos_list.append(ratio_CD11c_all[0])
+        # CD33
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD33_all, CD33_df, CD33_labels = ratioCalculation2(new_df, model_CD33)
+        CD33_Pos_list.append(ratio_CD33_all[0])
+        # CD152
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD152_all, CD152_df, CD152_labels = ratioCalculation2(new_df, model_CD152)
+        CD152_Pos_list.append(ratio_CD152_all[0])
+        # CD161
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD161_all, CD161_df, CD161_labels = ratioCalculation2(new_df, model_CD161)
+        CD161_Pos_list.append(ratio_CD161_all[0])
+        # CXCR5
+        new_df = copy.deepcopy(sample_df)
+        ratio_CXCR5_all, CXCR5_df, CXCR5_labels = ratioCalculation2(new_df, model_CXCR5)
+        CXCR5_Pos_list.append(ratio_CXCR5_all[0])
+        # CD183
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD183_all, CD183_df, CD183_labels = ratioCalculation2(new_df, model_CD183)
+        CD183_Pos_list.append(ratio_CD183_all[0])
+        # CD94
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD94_all, CD94_df, CD94_labels = ratioCalculation2(new_df, model_CD94)
+        CD94_Pos_list.append(ratio_CD94_all[0])
+        # CD127
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD127_all, CD127_df, CD127_labels = ratioCalculation2(new_df, model_CD127)
+        CD127_Pos_list.append(ratio_CD127_all[0])
+        # PD1
+        new_df = copy.deepcopy(sample_df)
+        ratio_PD1_all, PD1_df, PD1_labels = ratioCalculation2(new_df, model_PD1)
+        PD1_Pos_list.append(ratio_PD1_all[0])
+        # CD20
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD20_all, CD20_df, CD20_labels = ratioCalculation2(new_df, model_CD20)
+        CD20_Pos_list.append(ratio_CD20_all[0])
+        # CD16
+        new_df = copy.deepcopy(sample_df)
+        ratio_CD16_all, CD16_df, CD16_labels = ratioCalculation2(new_df, model_CD16)
+        CD16_Pos_list.append(ratio_CD16_all[0])
 
 
     pre_df = pd.DataFrame(CD3_Pos_list, columns=['CD3_Auto'])
@@ -156,6 +347,34 @@ if __name__ == '__main__':
     pre_df['CD56_Auto'] = CD56_Pos_list
     pre_df['gdTCR_Auto'] = gdTCR_Pos_list
     pre_df['CD8_Auto'] = CD8_Pos_list
+    pre_df['CD14_Auto'] = CD14_Pos_list
+    pre_df['Igd_Auto'] = Igd_Pos_list
+    pre_df['CD123_Auto'] = CD123_Pos_list
+    pre_df['CD85j_Auto'] = CD85j_Pos_list
+    pre_df['CD19_Auto'] = CD19_Pos_list
+    pre_df['CD25_Auto'] = CD25_Pos_list
+    pre_df['CD39_Auto'] = CD39_Pos_list
+    pre_df['CD27_Auto'] = CD27_Pos_list
+    pre_df['CD24_Auto'] = CD24_Pos_list
+    pre_df['CD45RA_Auto'] = CD45RA_Pos_list
+    pre_df['CD86_Auto'] = CD86_Pos_list
+    pre_df['CD28_Auto'] = CD28_Pos_list
+    pre_df['CD197_Auto'] = CD197_Pos_list
+    pre_df['CD11c_Auto'] = CD11c_Pos_list
+    pre_df['CD33_Auto'] = CD33_Pos_list
+    pre_df['CD152_Auto'] = CD152_Pos_list
+    pre_df['CD161_Auto'] = CD161_Pos_list
+    pre_df['CXCR5_Auto'] = CXCR5_Pos_list
+    pre_df['CD183_Auto'] = CD183_Pos_list
+    pre_df['CD94_Auto'] = CD94_Pos_list
+    pre_df['CD127_Auto'] = CD127_Pos_list
+    pre_df['PD1_Auto'] = PD1_Pos_list
+    pre_df['CD20_Auto'] = CD20_Pos_list
+    pre_df['CD16_Auto'] = CD16_Pos_list
     pre_df['id'] = [i[:11] for i in os.listdir(new_samples_path)]
-    pre_df = pre_df.reindex(columns=['id', 'CD3_Auto', 'CD4_Auto', 'CD57_Auto', 'CD56_Auto', 'gdTCR_Auto', 'CD8_Auto'])
+    pre_df = pre_df.reindex(columns=['id', 'CD3_Auto', 'CD4_Auto', 'CD57_Auto', 'CD56_Auto', 'gdTCR_Auto', 'CD8_Auto',
+                                     'CD14_Auto', 'Igd_Auto', 'CD123_Auto', 'CD85j_Auto', 'CD19_Auto', 'CD25_Auto',
+                                     'CD39_Auto', 'CD27_Auto', 'CD24_Auto', 'CD54RA_Auto', 'CD86_Auto', 'CD28_Auto',
+                                     'CD197_Auto', 'CD11c_Auto', 'CD33_Auto', 'CDC152_Auto', 'CD161_Auto', 'CXCR5_Auto',
+                                     'CD183_Auto', 'CD94_Auto', 'CD127_Auto', 'PD1_Auto', 'CD20_Auto', 'CD16_Auto'])
     pre_df.to_excel(output_path+'pre_df.xlsx', index=False)
